8084e849daa27a6a590e7c4a702dc3b595dadd63
Saalihou Ndiaye <saalihou@gmail.com>
2018-01-04 17:48:56 +0000

    fix: Randomize son selection

--- src/selectSons.js
+++ src/selectSons.js
@@ -1,19 +1,39 @@
+const _ = require('lodash');
+
 const Student = require('../models/Student');
 
 let currentOption = 'inf';
 
 module.exports = async function selectSons(isFlip) {
   currentOption = currentOption === 'inf' ? 'tr' : 'inf';
-  const students = await Student.find({ option: currentOption, level: 1 })
+  const count = await Student.find({ option: currentOption, level: 1 })
     .where('father')
     .exists(false)
-    .limit(2)
-    .exec();
+    .count();
+  const random1 = _.random(0, count - 2);
+  let random2 = _.random(0, count - 2);
+  while (random2 === random1 && count !== 1) {
+    random2 = _.random(0, count - 2);
+  }
+  const students = [
+    await Student.findOne({ option: currentOption, level: 1 })
+      .where('father')
+      .exists(false)
+      .skip(random1)
+      .limit(1)
+      .exec(),
+    await Student.findOne({ option: currentOption, level: 1 })
+      .where('father')
+      .exists(false)
+      .skip(random2)
+      .limit(1)
+      .exec()
+  ];
   if (students.length === 0 && !isFlip) {
     const otherOptionStudents = await selectSons(true);
     return otherOptionStudents;
   } else if (students.length === 1) {
-    const otherStudent = await await Student.findOne({
+    const otherStudent = await Student.findOne({
       option: currentOption,
       level: 1
     }).exec();
